b274f7554e957b0bb06cf6d38a7fb64a99e0ea8d
Akshat Sharma <akshatsharma@Akshats-MacBook-Pro.local>
2018-05-26 12:25:23 +0400

    Setup

--- main.go
+++ main.go
@@ -10,3 +10,25 @@ import
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+var (
+	API_KEY = 
+)
+
+type Request struct{
+	ID int 'json:"id"'
+}
+
+type MovieDBResponse struct{
+	Movies []Movie 'json:"results"'
+}
+
+type Movie struct{
+	Title string 'json:"title"'
+	Description string 'json:"overview"'
+	Cover string 'json:"poster_path"'
+	ReleaseData string 'json:"release_data"'
+}
+
+func Handler(request Request) ([]Movie, error){
+}
